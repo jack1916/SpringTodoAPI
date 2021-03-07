19bfbbcdc1504a330ef5f0512fa3c1e84cf1c72d
jackholsgrove <jackholsgrove@gmail.com>
2021-03-07 19:59:49 +0000

    Swapped around delete and patch endpoints to use PathVariable

--- src/main/java/com/jack/todo/TodoController.java
+++ src/main/java/com/jack/todo/TodoController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,13 +30,13 @@ public class TodoController {
 	}
 	
 	
-	@PatchMapping("/todos")
-	public TodoItem update(@RequestBody final TodoItem todo){
-		return todoService.complete(todo.getId());
+	@PatchMapping("/todos/{id}")
+	public TodoItem update(@PathVariable final String id){
+		return todoService.complete(id);
 	}
 	
-	@DeleteMapping("/todos")
-	public void delete(@RequestBody final TodoItem todo) {
-		todoService.delete(todo.getId());
+	@DeleteMapping("/todos/{id}")
+	public void delete(@PathVariable final String id) {
+		todoService.delete(id);
 	}
 }
