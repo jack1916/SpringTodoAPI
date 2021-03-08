96a69227482d79706d9adc5d82dcbc76acec159a
jackholsgrove <jackholsgrove@gmail.com>
2021-03-08 19:33:25 +0000

    Cleaned up service tests

--- src/test/java/com/jack/todo/TodoServiceImplTests.java
+++ src/test/java/com/jack/todo/TodoServiceImplTests.java
@@ -35,15 +35,17 @@ public class TodoServiceImplTests {
 	public void TodoServiceFindAll_WillRunTest(){
 		// When findAll is ran
 		todoService.findAll();
+		
 		// Expect findAll to be ran in the repository
 		Mockito.verify(mockRepository).findAll();
 	}
 	
 	@Test
-	// 
+	// create should return a todo with an ID and run create method in mocked repository
 	public void TodoServiceCreate_ReturnsTodoTest() {
 		// When create is ran
 		final TodoItem result = todoService.create(MY_TODO);
+		
 		// Expect return of same object
 		assertEquals(result.getTitle(),MY_TODO.getTitle());
 		// Expect returned object to have a UUID
@@ -53,17 +55,21 @@ public class TodoServiceImplTests {
 	}
 	
 	@Test
+	// delete should run the delete method in the mocked repository
 	public void TodoServiceDelete_WillRunTest() {
 		// When delete is ran
 		todoService.delete(MY_TODO.getId());
+		
 		// Expect delete method to be ran in repository
 		Mockito.verify(mockRepository).delete(any());
 	}
 	@Test
+	// complete should return a TodoItem with complete set to true and update should be ran
 	public void TodoServiceComplete_ReturnsTodoTest() {
 		// when 
 		when(mockRepository.findById(id)).thenReturn(MY_TODO);
 		final TodoItem result = todoService.complete(id);
+		
 		// Expect update method to be ran in repository
 		Mockito.verify(mockRepository).update(any());
 		// Expect complete to be true
